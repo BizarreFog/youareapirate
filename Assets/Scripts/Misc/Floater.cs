7bfb64c53c0ee4c2896bde6d770b5bed48f65ca6
TomasAbrams <TSAbrams3039@muleriders.saumag.edu>
2021-01-30 13:59:49 -0600

    WIP Ship and Player

---
+++ Assets/Scripts/Misc/Floater.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class Floater : MonoBehaviour
+{
+    private Rigidbody rigidBody;
+    public float depthBeforeSubmerged = 1f;
+    public float displacementAmount = 3f;
+
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        if(transform.position.y < 0f)
+        {
+            float displacementMultiplier = Mathf.Clamp01(-transform.position.y / depthBeforeSubmerged) * displacementAmount;
+            rigidBody.AddForce(new Vector3(0f, Mathf.Abs(Physics.gravity.y) * displacementMultiplier, 0f), ForceMode.Acceleration);
+
+        }
+    }
+
+}
---
+++ Assets/Scripts/Player/ShipController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class ShipController : MonoBehaviour
+{
+
+    public float bobHeight = .25f;
+    public float bobSpeed = .25f;
+
+    private float originalYpos = 0;
+
+    void Start()
+    {
+        originalYpos = this.transform.localPosition.y;
+    }
+
+    void Update()
+    {
+        ShipBobbing();
+    }
+
+    void ShipBobbing()
+    {
+        this.transform.localPosition = new Vector3(this.transform.localPosition.x , Mathf.Sin(Time.time * bobSpeed) * bobHeight + originalYpos, this.transform.localPosition.z);
+    }
+}
